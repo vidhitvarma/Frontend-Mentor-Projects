c10e9b43d92ba4a7d9f7ab4201081f10976e74e9
Vidhit Varma <vidhitvarma300@gmail.com>
2022-10-27 19:04:16 +0530

    Adding JS validation

--- ping-coming-soon-page-master/app.js
+++ ping-coming-soon-page-master/app.js
@@ -1,2 +1,36 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
+
+//functions
+
+function isValidEmail(email){
+  const emailRegex =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;;
+  return emailRegex.test(String(email).toLowerCase());
+}
+
+function showError(input,message){
+  const label = input.parentElement;
+  label.classList.remove('success');
+  label.classList.add('error');
+  const errorMsg = label.querySelector('small');
+  errorMsg.innerText = message;
+}
+
+function noError(input){
+  const label = input.parentElement;
+  label.classList.remove('error');
+  label.classList.add('success');
+}
+
+form.addEventListener('submit',function(e){
+  e.preventDefault();
+  if(email.value === ''){
+    showError(email,"Email cannot be empty");
+  }else if(!isValidEmail(email.value)){
+    showError(email, "Please provide a valid email address");
+  }
+  else{
+    noError(email)
+  }
+});
