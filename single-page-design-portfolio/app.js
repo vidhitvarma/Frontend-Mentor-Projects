45f8c68cc607375e1b73980a3281afe98c34c7c2
Vidhit Varma <vidhitvarma300@gmail.com>
2023-01-08 00:48:30 +0530

    adding desktop changes

--- single-page-design-portfolio/app.js
+++ single-page-design-portfolio/app.js
@@ -4,15 +4,13 @@ const rightBtn = document.getElementById('right-btn');
 const image = document.querySelectorAll('#carousel__images img')
 let index = 2;
 
-
 function changeImg(){
   if(index > image.length - 1){
     index = 0;
   }else if( index < 0){
     index = image.length - 1;
   }
-
-  imgContainer.style.transform = `translateX(${-index * 270}px)`;
+  imgContainer.style.transform = `translateX(${-index * (image[index].width + 16)}px)`;
 }
 
 leftBtn.addEventListener('click', function(){
